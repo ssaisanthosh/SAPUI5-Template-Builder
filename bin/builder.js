d86e1c000470b79716ad3ee9a4030d5cd80e47fa
sai <me@ssaisanthosh.com>
2020-08-26 22:15:32 +0530

    26-08-2020

--- bin/builder.js
+++ bin/builder.js
@@ -1,8 +1,11 @@
 "use strict";
-const fs = require("fs");
+const fs = require("fs"),
+path = require('path');
 // replaceString = require("string-replace-async");
-exports.file = function (source, file, project, namespace, viewname) {
-  fs.readFile("./template/" + source + ".tbs", "utf8", (err, data) => {
+exports.file = function (source, file, project, namespace, viewname) {  
+  var sPath = path.dirname(fs.realpathSync(__filename));
+  sPath = sPath.substring(0, sPath.lastIndexOf('/'));
+  fs.readFile(sPath + "/template/" + source + ".tbs", "utf8", (err, data) => {
     if (err) {
       console.error(err);
       return;
